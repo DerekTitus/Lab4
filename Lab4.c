8d7068b252deef45872659ddf915ae62b46f44fe
Derek Titus <c15derek.titus@usafa.edu>
2013-10-22 15:07:31 -0600

    the beginning

---
+++ Lab4.c
@@ -0,0 +1,14 @@
+/*
+ * Lab4.c
+ *
+ *  Created on: Oct 22, 2013
+ *      Author: C15Derek.Titus
+ */
+
+#ifndef LAB4_C_
+#define LAB4_C_
+
+
+
+
+#endif /* LAB4_C_ */
---
+++ Lab4.h
@@ -0,0 +1,14 @@
+/*
+ * Lab4.h
+ *
+ *  Created on: Oct 22, 2013
+ *      Author: C15Derek.Titus
+ */
+
+#ifndef LAB4_H_
+#define LAB4_H_
+
+
+
+
+#endif /* LAB4_H_ */
