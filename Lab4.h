d3e7abff8a329c5960409df76dca347dfad4a0b6
Derek Titus <c15derek.titus@usafa.edu>
2013-10-23 21:04:06 -0600

    delays

--- Lab4.h
+++ Lab4.h
@@ -8,6 +8,20 @@
 #ifndef LAB4_H_
 #define LAB4_H_
 
+void INITSPI();
+
+void INITLCD();
+
+void LCDCLEAR;
+
+void changeLine;
+
+void writeChar(char singleChar);
+
+void writeString(char*string1);
+
+void scrollString(char*string2, char*string3);
+
 
 
 
