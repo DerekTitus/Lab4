b21a119a028842a1b0d415b60569d2a331a00ca6
Derek Titus <c15derek.titus@usafa.edu>
2013-10-23 21:39:58 -0600

    I wrote the code for write 4 and write 8. There are no errors in the .c file at the moment. I also realized that I was using a file named lab.c instead of lab4.c. I fixed the issue and things should be on track now

--- Lab4.c
+++ Lab4.c
@@ -8,6 +8,144 @@
 #ifndef LAB4_C_
 #define LAB4_C_
 
+#include "Lab4.h"
+#include <msp430.h>
+#define RS_MASK 0x40
+
+unsigned char LCDCON;
+
+
+void initSPI()
+{
+	UCB0CTL1 |= UCSWRST;
+	UCB0CTL0 |= UCCKPH|UCMSB|UCMST|UCSYNC;
+	UCB0CTL1 |= UCSSEL1;
+	UCB0STAT |= UCLISTEN;
+
+	P1SEL |= BIT5;
+	P1SEL2 |= BIT5;
+	P1SEL |= BIT6;
+	P1SEL2 |= BIT6;
+	P1SEL |= BIT7;
+	P1SEL2 |= BIT7;
+
+	UCB0CTL1 &= UCSWRST;
+
+
+}
+
+void SS_HI()
+{
+	P1DIR |= BIT0;
+	P1OUT |= BIT0;
+}
+
+void SS_LOW()
+{
+	P1DIR |= BIT0;
+	P1OUT &= BIT0;
+}
+
+void longdelay()
+{
+	__delay_cycles(1760);
+}
+
+void shortdelay()
+{
+	__delay_cycles(44);
+}
+
+void LCDwrite4(char NibbletoSend)
+{
+	unsigned char sendNibble = NibbletoSend;
+	sendNibble &= 0x0f;
+	sendNibble |= LCDCON;
+
+	sendNibble &= 0x7f;
+	SPISend(sendNibble);
+	shortdelay();
+
+	sendNibble |= 0x00;
+	SPISend(sendNibble);
+	shortdelay();
+
+	sendNibble &= 0x7f;
+	SPISend(sendNibble);
+	shortdelay();
+}
+
+
+void LCDwrite8(char byteToSend)
+{
+    unsigned char sendByte = byteToSend;
+
+    sendByte &= 0xF0;
+
+    sendByte = sendByte >> 4;               // rotate to the right 4 times
+
+    LCDwrite4(sendByte);
+
+    sendByte = byteToSend;
+
+    sendByte &= 0x0F;
+
+    LCDwrite4(sendByte);
+}
+
+
+
+
+void LCDinit()
+{
+
+
+}
+
+void writeCommandNibble(char commandNibble)
+{
+    LCDCON &= ~RS_MASK;
+    LCDwrite4(commandNibble);
+    longdelay();
+}
+
+void writeCommandByte(char commandByte)
+{
+    LCDCON &= ~RS_MASK;
+    LCDwrite8(commandByte);
+    longdelay();
+}
+
+void writeDataByte(char dataByte)
+{
+    LCDCON |= RS_MASK;
+    LCDwrite8(dataByte);
+    longdelay();
+}
+
+
+
+void SPI_send(char byteToSend)
+{
+    char readByte;
+
+    SS_LOW();
+
+    UCB0TXBUF = byteToSend;
+
+    while(!(UCB0RXIFG & IFG2))
+    {
+        // wait until you've received a byte
+    }
+
+    readByte = UCB0RXBUF;
+
+    SS_HI();
+}
+
+
+
+
 
 
 
